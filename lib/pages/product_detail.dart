c5b575b62b05e4fabfb1745d0030354eb0bb212a
dj-hirrot <hirotoshibutani@gmail.com>
2020-12-09 04:31:51 +0900

    update: customize detail page

--- lib/pages/product_detail.dart
+++ lib/pages/product_detail.dart
@@ -8,15 +8,52 @@ class ProductDetail extends StatelessWidget {
   Widget build(BuildContext context) {
     final Product product = ModalRoute.of(context).settings.arguments;
     return Scaffold(
-      appBar: AppBar(
-        title: Text('商品詳細'),
-      ),
-      body: Container(
-        child: Center(
-          child: Text(
-              product.title
-          ),
+        appBar: AppBar(
+          title: Text('商品詳細'),
         ),
+        body: _body(context, product),
+    );
+  }
+
+  Widget _body(BuildContext context, Product product) {
+    return SingleChildScrollView(
+        child: Column(children: <Widget>[
+      Center(
+        child: Image.network(product.sampleImageUrl),
+      ),
+      Text(
+        product.title,
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      Text(product.item.humanizeName),
+      Text("¥${product.price.toString()}"),
+      Text("Made by ${product.material.user.name}"),
+      product.material.description.isEmpty
+          ? Container()
+          : _descriptionSection(context, product),
+    ]));
+  }
+
+  Widget _descriptionSection(BuildContext context, Product product) {
+    return Container(
+      margin: EdgeInsets.only(top: 16.0),
+      child: Column(
+        children: <Widget>[
+          Text(
+            "Describe this item",
+            style: TextStyle(
+              color: Colors.grey,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(16.0),
+            child: Text(
+              product.material.description,
+            ),
+          ),
+        ],
       )
     );
   }
